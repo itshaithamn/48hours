d78d6b3d5ac9a4374d55a483cab0cdbf1325e08f
gabihasaj1 <ghasaj@oakland.edu>
2025-01-25 13:03:54 -0500

    Playground

--- static/react-flask-app/src/components/Playground.js
+++ static/react-flask-app/src/components/Playground.js
@@ -1,13 +1,71 @@
-import React, {useState} from 'react'
-import Navbar from './Navbar'
+import React, { useEffect, useRef } from 'react';
 
-const Playground = () => {
-    return(
-        <div>
-            <Navbar />
-            <h1>Playground Page</h1>
-        </div>
-    )
+
+function Playground() {
+
+  const containerRef = useRef(null);
+  const boxRef = useRef(null);
+
+  const isClicked = useRef(false);
+
+  const coords = useRef({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0
+  });
+
+  useEffect(() => {
+    if (!boxRef.current || !containerRef.current) return;
+
+    const box = boxRef.current;
+    const container = containerRef.current;
+
+    const onMouseDown = (e) => {
+      isClicked.current = true;
+      coords.current.startX = e.clientX;
+      coords.current.startY = e.clientY;
+    }
+
+    const onMouseUp = (e) => {
+      isClicked.current = false;
+      coords.current.lastX = box.offsetLeft;
+      coords.current.lastY = box.offsetTop;
+    }
+
+    const onMouseMove = (e) => {
+      if (!isClicked.current) return;
+
+      const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+      const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+      box.style.top = `${nextY}px`;
+      box.style.left = `${nextX}px`;
+    }
+
+    box.addEventListener('mousedown', onMouseDown);
+    box.addEventListener('mouseup', onMouseUp);
+    container.addEventListener('mousemove', onMouseMove);
+    container.addEventListener('mouseleave', onMouseUp);
+
+    const cleanup = () => {
+      box.removeEventListener('mousedown', onMouseDown);
+      box.removeEventListener('mouseup', onMouseUp);
+      container.removeEventListener('mousemove', onMouseMove);
+      container.removeEventListener('mouseleave', onMouseUp);
+    }
+
+    return cleanup;
+  }, [])
+
+  return (
+    <div className="main">
+        <h1>Playground</h1>
+      <div ref={containerRef} className="container">
+        <div ref={boxRef} className="box"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Playground
+export default Playground;
