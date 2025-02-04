74cf3995da1529838df79331fbdca6f27fa81135
itshaithamn <hnaksho@oakland.edu>
2025-02-03 23:14:30 -0500

    added nodeCreate.py, need to figure out how to combine it all, added userData.js comments, added app.py comments

--- api/app.py
+++ api/app.py
@@ -8,3 +8,17 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
+
+
+# from flask import Flask, request, jsonify
+#
+# app = Flask(__name__)
+#
+# @app.route('/receive_data', methods=['POST'])
+# def receive_data():
+#     data = request.json  # Get JSON data from request
+#     print("Received Data:", data)
+#     return jsonify({"message": "Data received", "data": data})
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
