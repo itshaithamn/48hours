0ee4c7be72465e6b3e9555db5faae04b898c789c
dmeyer4569 <dmeyer2@oakland.edu>
2025-03-01 16:35:46 -0500

    upload.py is ready to go author = 5 is a placeholder for later

--- simulation/download.py
+++ simulation/download.py
@@ -3,13 +3,19 @@ import socket
 import struct
 import os
 
-user_id = 5
+userid = 5
 host_ip = "107.21.218.63"
 port = 51832
 client_socket = None
+'''
+Leaving the local_output_dor blank.. Instructions to use:
+Windows: path\\to\\local\\file_save_spot
+linux/mac: path/to/local/file_save_spot
+'''
 output_dir = "C:\\Users\\Devin\\Desktop\\OU\\Winter25\\CSI2999"
 
 class MariaDBFileDownload():
+
     def __init__(self, host="107.21.218.63", user="FourtyEightHours", password="test1234", database="CSI2999"):
         """Connect to the mariaDB database"""
         self.conn = mysql.connector.connect(
@@ -77,31 +83,24 @@ class MariaDBFileDownload():
                 print(f"[+] Downloaded {filename} successfully")
             except Exception as e:
                 print(f"[!] Error while downloading: {e}")
-        """
-        print("[+] Now moving on to receiving - 2")
-        file_path_received_data_len = client_socket.recv(4)
-        print(f"[+] Received {file_path_received_data_len}")
 
-        # receive filepath
-        filepath_len_received = struct.unpack('!I', file_path_received_data_len)[0]
-        filepath_received = client_socket.recv(filepath_len_received).decode()
-        print(f"[+] Received request to download user file: {filepath_received}")
-        """
 
     def terminate_connection(self):
-        print("[+] Connection closing")
+        """Used to close connection"""
         if client_socket:
             print("[+] Successfully closed connection")
             client_socket.close()
+
+
 #TEST
 if __name__ == "__main__":
     db = MariaDBFileDownload()
     #set up the connection to the server
 
     #Using user_id 5 for now.. Change later when the time comes
-    saved_user_files_list = db.get_saved_user_files_from_database(user_id)
+    saved_user_files_list = db.get_saved_user_files_from_database(userid)
     #Change later if needed. The output shouldn't end up print in the end...
-    #print(f"User: saved_user_files_list: {user_id}, Filename: {saved_user_files_list[0][0]}, Filepath: {saved_user_files_list[0][1]}, File Date: {saved_user_files_list[0][2]}, File Size: {saved_user_files_list[0][3]}")
+    print(f"User: saved_user_files_list: {userid}, Filename: {saved_user_files_list[0][0]}, Filepath: {saved_user_files_list[0][1]}, File Date: {saved_user_files_list[0][2]}, File Size: {saved_user_files_list[0][3]}")
 
 
     db.download_user_file('/var/lib/mariadb_files/test_file2.txt', host_ip, port)
