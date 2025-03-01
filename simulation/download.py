7a1e097d4f402a84e779bc74b6f0613359eee989
dmeyer4569 <dmeyer2@oakland.edu>
2025-03-01 16:46:43 -0500

    upload.py is ready to go author = 5 is a placeholder for later
    download.py works!!
    
    These files might need minor modifications once in a server environment
    
    There are 2 files on the aws instance, need to fix Address already in use error after the code is run once

--- simulation/download.py
+++ simulation/download.py
@@ -12,7 +12,7 @@ Leaving the local_output_dor blank.. Instructions to use:
 Windows: path\\to\\local\\file_save_spot
 linux/mac: path/to/local/file_save_spot
 '''
-output_dir = "C:\\Users\\Devin\\Desktop\\OU\\Winter25\\CSI2999"
+output_dir = ""
 
 class MariaDBFileDownload():
 
@@ -100,10 +100,9 @@ if __name__ == "__main__":
     #Using user_id 5 for now.. Change later when the time comes
     saved_user_files_list = db.get_saved_user_files_from_database(userid)
     #Change later if needed. The output shouldn't end up print in the end...
-    print(f"User: saved_user_files_list: {userid}, Filename: {saved_user_files_list[0][0]}, Filepath: {saved_user_files_list[0][1]}, File Date: {saved_user_files_list[0][2]}, File Size: {saved_user_files_list[0][3]}")
+    print(saved_user_files_list)
 
-
-    db.download_user_file('/var/lib/mariadb_files/test_file2.txt', host_ip, port)
+    db.download_user_file('/var/lib/mariadb_files/5/test_file2.txt', host_ip, port)
 
     #After all is done, terminate connection, ONLY CALL AFTER
     db.terminate_connection()
